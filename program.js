cde46bef8eb9d85a5667ec64a1d095ce2bef09c3
Dev Server <developer@office.mediasuite.co.nz>
2014-10-17 12:49:01 +1300

    Lines

--- program.js
+++ program.js
@@ -1,9 +1,18 @@
 'use strict';
 
 var through = require('through');
+var split = require('split');
+var odd = true;
 
-function write (buf) { 
-	this.queue(buf.toString().toUpperCase())
+function write (buf) {
+	var str = buf.toString()
+	if (!odd) { 
+		str = str.toUpperCase()
+	} else {
+		str = str.toLowerCase()
+	}
+	odd = !odd;
+	this.queue(str + '\n')
 }
 function end () { 
 	this.queue(null)
@@ -11,4 +20,6 @@ function end () {
 
 var tr = through(write, end);
 
-process.stdin.pipe(tr).pipe(process.stdout);
+process.stdin
+	.pipe(split())
+	.pipe(tr).pipe(process.stdout);
