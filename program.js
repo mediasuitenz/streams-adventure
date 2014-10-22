bc45ffd1b6afe1ef9d2fcf7b7e4e87a43769bc28
Mark Stuart <mstuart_is@me.com>
2014-10-23 12:46:13 +1300

    http-client

--- program.js
+++ program.js
@@ -1,18 +1,6 @@
 'use strict';
 
-var http = require('http');
-var through = require('through')
+var request = require('request');
 
-function write (buf) { 
-	this.queue(buf.toString().toUpperCase()) 
-}
-function end () { this.queue(null) }
-
-var server = http.createServer(function (req, res) {
-    if (req.method === 'POST') {
-        req.pipe(through(write, end)).pipe(res);
-    }
-    res.end('beep boop\n');
-});
-
-server.listen(process.argv[2]);
+var r = request.post('http://localhost:8000/');
+process.stdin.pipe(r).pipe(process.stdout);
